bd43704424b2d15d61be65f9908aa4e4caea0735
Enricco Diniz <enriccoamaral@hotmail.com>
2025-08-29 09:34:32 -0300

    Teste de SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["https://public-tv-productions.s3.sa-east-1.amazonaws.com"],
     unoptimized: false,
   },
 };
