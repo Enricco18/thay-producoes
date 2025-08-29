e0007dde38088134f30d641ce804fa5ab3315645
Enricco Diniz <enriccoamaral@hotmail.com>
2025-08-29 09:48:01 -0300

    Teste de SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["https://public-tv-productions.s3.sa-east-1.amazonaws.com"],
+    domains: ["public-tv-productions.s3.sa-east-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'public-tv-productions.s3.sa-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
     unoptimized: false,
   },
 };
